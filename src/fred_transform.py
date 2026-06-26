99d63fbbe3a41b52fcf1f2ad1fd18a1f6f7b667c
donn-majidi <donn.majidi@gmail.com>
2026-06-26 16:32:07 +0200

    Feature: The function fred_transform can now handle both FRED-QD and FRED-MD datasets.

--- src/fred_transform.py
+++ src/fred_transform.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
+import warnings
 
 __transform_codes__ = [1,2,3,4,5,6,7]
+__frequencies__ = ['QD', 'MD']
 
-def fred_transform (data: pd.Series, transform_code: int):
+def _fred_series_transform (data: pd.Series, transform_code: int):
     """
     This function transforms the input data to achieve stationarity.
     Transformations are based on integer codes which determin the
@@ -17,7 +19,7 @@ def fred_transform (data: pd.Series, transform_code: int):
                     == 6 -> Second order log difference.
                     == 7 -> Relative difference.
                     
-    [Reference: FRED-QD: A Quarterly Database for Macroeconomic Research McCracken and Ng, 2020]
+    Reference: FRED-QD: A Quarterly Database for Macroeconomic Research McCracken and Ng, 2020.
 
     Parameters
     ----------
@@ -39,7 +41,7 @@ def fred_transform (data: pd.Series, transform_code: int):
     """
     
     if transform_code not in __transform_codes__:
-        raise ValueError(f"Transformation code {transform_code} is invalid. Please choose from {__transform_codes__}")
+        raise ValueError(f"Transformation code {transform_code} is invalid. It has to be chosen from {__transform_codes__}")
         
     match transform_code:
         case 1:
@@ -64,9 +66,8 @@ def fred_transform (data: pd.Series, transform_code: int):
             data_tr = data/data.shift(1) - 1
             
     return data_tr
-
-
-def fred_qd_transform(data: pd.DataFrame):
+            
+def fred_transform(data: pd.DataFrame, freq: str):
     """
     This function transforms the input data to achieve stationarity.
     Transformations are based on integer codes which determin the
@@ -80,17 +81,84 @@ def fred_qd_transform(data: pd.DataFrame):
                     == 6 -> Second order log difference.
                     == 7 -> Relative difference.
                     
-    [Reference: FRED-QD: A Quarterly Database for Macroeconomic Research McCracken and Ng, 2020]
+    Reference: FRED-QD: A Quarterly Database for Macroeconomic Research McCracken and Ng, 2020.
+
+    Parameters
+    ---------- freq: str
+    data : pd.DataFrame
+        Raw FRED-QD or FRED-MD data frame.
+        
+    freq : str
+        String indicating the frequency of the input data. It has to be either "QD" for quarterly data or "MD" for monthly data.
+
+    Returns
+    -------
+    Transformed series in a pd.DataFrame object.
+    
+    Raises
+    ------
+    ValueError
+        If frequency is not recognized.
+
+    """
+    
+    if freq not in __frequencies__:
+        raise ValueError(f"Unrecognized frequency {freq}. Choose 'QD' for quarterly data or 'MD' for monthly data.")
+    
+    transformed = []
+    
+    if freq == 'QD':
+        init_idx = 2
+        trcode_idx = 1
+        
+    elif freq == 'MD':
+        init_idx = 1
+        trcode_idx = 0
+    
+    for i in range(1,len(data.columns)):
+        
+        input_series = data.iloc[init_idx:,i]
+        transform_code = data.iloc[trcode_idx,i]
+        input_dates = data.iloc[init_idx:,0]
+        
+        _input_series_tr = _fred_series_transform(input_series, transform_code)
+        _input_series_tr.name = data.columns[i]
+        _input_series_tr.index = pd.to_datetime(input_dates, format="%m/%d/%Y", errors='coerce')
+
+        transformed.append(_input_series_tr)
+        
+    transformed_df = pd.concat(transformed, axis=1, join='outer')
+    
+    return transformed_df       
+
+def fred_qd_transform(data: pd.DataFrame):
+    warnings.warn('This function is deprecated and will probably be removed in a future release. Use fred_transform instead.',
+                  DeprecationWarning,
+                  stacklevel=2)
+    """
+    This function transforms the input data to achieve stationarity.
+    Transformations are based on integer codes which determin the
+    type of transformation.
+    
+    transform_code  == 1 -> No transformation.
+                    == 2 -> First difference.
+                    == 3 -> Second order difference.
+                    == 4 -> Log transform.
+                    == 5 -> Log difference.
+                    == 6 -> Second order log difference.
+                    == 7 -> Relative difference.
+                    
+    Reference: FRED-QD: A Quarterly Database for Macroeconomic Research McCracken and Ng, 2020.
 
     Parameters
     ----------
     data : pd.DataFrame
         Raw FRED-QD data input.
-
+        
     Returns
     -------
     Transformed series in a pd.DataFrame object.
-
+    
     """
     
     transformed = []
@@ -131,23 +199,3 @@ def fred_qd_transform(data: pd.DataFrame):
     transformed_df = pd.concat(transformed, axis=1, join='outer')
     
     return transformed_df
-
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
